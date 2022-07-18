f302a57251bc9233f1cb4d7f4032328a9fde43eb
Tatu-Armadillo <prohazor.fa@gmail.com>
2022-07-18 15:44:01 -0300

    first commit

---
+++ src/test/java/br/com/challenge/bit/BitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.challenge.bit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
